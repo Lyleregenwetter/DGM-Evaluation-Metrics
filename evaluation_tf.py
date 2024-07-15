432f069545636febf9867ad9ea916564e9ce76de
Lyle Regenwetter <regenwet@mit.edu>
2024-07-15 14:50:43 -0400

    Remove tf dependency from utils
    (Example models are still written in TF)

--- evaluation_tf.py
+++ evaluation_tf.py
@@ -1,3 +1,6 @@
+# This is the old version of the evals file, which was used in the paper
+# The TensorFlow dependency has been removed for the new version, but the models still use TF. 
+
 from pymoo.indicators.hv import HV
 from pymoo.indicators.gd import GD
 import numpy as np
@@ -6,10 +9,25 @@ import os
 from scipy.spatial.distance import pdist, squareform
 from scipy.spatial import ConvexHull
 import sklearn
+import random
 from tqdm import trange
+import tensorflow as tf
 import eval_prd
+import importlib
+importlib.reload(eval_prd)
 
-from scipy.spatial.distance import cdist
+#Each of these evlauation metrics are contained in a wrapper function and structured to be called using the 
+#same variables so that they can be called in loop. The wrapper function returns the callable action. 
+#Inputs are: 
+#x_eval = design space values of generated designs, 
+#y_eval = performance space values of generate designs, 
+#x_data = design space values of original data
+#y_data = performance space values of original data
+#n_data = design space values of invalid samples
+#scorebars = whether to show progress bars and staus updates
+
+#Any constants are passed to the function in optional arguments when the wrapper function is called
+
 
 def Hypervolume_wrapper(hv_ref="auto"):
     def Hypervolume(x_eval, y_eval, x_data, y_data, n_data, scorebars, hv_ref=hv_ref):
@@ -49,6 +67,7 @@ def get_perc_band(value, data, band):
     ub = np.quantile(data, upper)
     mask = np.logical_and(data>=lb, data<=ub)
     return mask
+       
 
 def calc_distance(X, Y, distance="Euclidean"):
     if distance=="Euclidean":
@@ -56,8 +75,16 @@ def calc_distance(X, Y, distance="Euclidean"):
     else:
         raise Exception("Unknown distance metric specified")
         
+        
 def L2_vectorized(X, Y):
-    return cdist(X, Y, 'euclidean')
+    #Vectorize L2 calculation using x^2+y^2-2xy
+    X_sq = np.sum(np.square(X), axis=1)
+    Y_sq = np.sum(np.square(Y), axis=1)
+    sq = np.add(np.expand_dims(X_sq, axis=-1), np.transpose(Y_sq)) - 2*np.matmul(X,np.transpose(Y))
+    sq = np.clip(sq, 0.0, 1e12)
+    return np.sqrt(sq)
+
+
 
 def signed_distance_to_boundary_wrapper(direction, ref, p_, method="linear"):
     def signed_distance_to_boundary(x_eval, y_eval, x_data, y_data, n_data, scorebars, direction=direction, ref=ref, p_=p_, method=method):
@@ -79,15 +106,17 @@ def signed_distance_to_boundary_wrapper(direction, ref, p_, method="linear"):
         zeros = np.expand_dims(np.zeros(np.shape(diff)[1]), axis=0)
         dists_clip = L2_vectorized(diff_clip, zeros)
         dists = diff_sc.min(axis=1, keepdims=True)
-        dists_mask = np.all(diff > 0, axis=1)
-        final_scores = np.multiply(dists_mask, dists)-dists_clip
-        return final_scores, np.mean(final_scores)
+        dists_mask = tf.reduce_all(tf.math.greater(diff, 0), axis=1)
+        dists_mask = tf.expand_dims(tf.cast(dists_mask, "float32"), axis=1)
+        final_scores = tf.multiply(dists_mask, dists)-dists_clip
+        return final_scores, tf.reduce_mean(final_scores)
     return signed_distance_to_boundary
         
 def gen_gen_distance_wrapper(flag, reduction="min", distance="Euclidean"):
     def gen_gen_distance(x_eval, y_eval, x_data, y_data, n_data, scorebars, flag=flag):
         if scorebars:
             print("Calculating Gen-Gen Distance")
+        scores = []
         if flag == "x":
             x = x_eval
         elif flag == "y":
@@ -97,20 +126,21 @@ def gen_gen_distance_wrapper(flag, reduction="min", distance="Euclidean"):
         else:
             raise Exception("Unknown flag passed")
         res = calc_distance(x, x, distance)
-        np.fill_diagonal(res, np.max(res))
+        res = tf.linalg.set_diag(res, tf.reduce_max(res, axis=1))
         if reduction == "min":
-            scores = np.min(res, axis=1)
+            scores = tf.reduce_min(res, axis=1)
         elif reduction == "ave":
-            scores = np.mean(res, axis=1)
+            scores = tf.reduce_mean(res, axis=1)
         else:
             raise Exception("Unknown reduction method")
-        return scores, np.mean(scores)
+        return scores, tf.reduce_mean(scores)
     return gen_gen_distance
 
 def distance_to_centroid_wrapper(flag, distance="Euclidean"):
     def distance_to_centroid(x_eval, y_eval, x_data, y_data, n_data, scorebars, flag=flag):
         if scorebars:
             print("Calculating Distance to Centroid")
+        scores = []
         if flag == "x":
             x = x_eval
         elif flag == "y":
@@ -122,11 +152,13 @@ def distance_to_centroid_wrapper(flag, distance="Euclidean"):
         centroid = np.mean(x, axis=0)
         vec = np.subtract(x, centroid)
         distance = np.linalg.norm(vec, axis=1)
-        return distance, np.mean(distance)
+        return scores, tf.reduce_mean(distance)
     return distance_to_centroid
+
 
 def DPP_diversity_wrapper(flag, subset_size=10):
     def DPP_diversity(x_eval, y_eval, x_data, y_data, n_data, scorebars, flag=flag):
+        # Average log determinant
         if flag == "x":
             x = x_eval
         elif flag == "y":
@@ -135,21 +167,27 @@ def DPP_diversity_wrapper(flag, subset_size=10):
             x = pd.concat([x_eval, y_eval], axis=0)
         else:
             raise Exception("Unknown flag passed")
-        r = np.sum(np.square(x), axis=1, keepdims=True)
-        D = r - 2 * np.dot(x, x.T) + r.T
-        S = np.exp(-0.5 * np.square(D))
+        x = tf.convert_to_tensor(x, dtype="float32")
+        r = tf.reduce_sum(tf.math.square(x), axis=1, keepdims=True)
+        D = r - 2 * tf.matmul(x, tf.transpose(x)) + tf.transpose(r)
+        S = tf.exp(-0.5 * tf.math.square(D))
+        y = tf.ones(np.shape(x)[0])
         try:
-            eig_val, _ = np.linalg.eigh(S)
+            eig_val, _ = tf.linalg.eigh(S)
         except: 
-            eig_val = np.ones(x.shape[0])
-        loss = -np.mean(np.log(np.maximum(eig_val, 1e-7)))
+            eig_val = tf.ones_like(y)
+        loss = -tf.reduce_mean(tf.math.log(tf.math.maximum(eig_val, 1e-7)))
         return None, loss
     return DPP_diversity
+
+
 
 def gen_data_distance_wrapper(flag, reduction="min", distance="Euclidean"):
     def gen_data_distance(x_eval, y_eval, x_data, y_data, n_data, scorebars, flag = flag):
         if scorebars:
             print("Calculating Gen-Data Distance")
+        scores = []
+        
         if flag == "x":
             x = x_eval
             data = x_data
@@ -161,21 +199,23 @@ def gen_data_distance_wrapper(flag, reduction="min", distance="Euclidean"):
             data = pd.concat([x_data, y_data], axis=0)
         else:
             raise Exception("Unknown flag passed")
+        num_eval = np.shape(x)[0]
         res = calc_distance(x, data, distance)
 
         if reduction == "min":
-            scores = np.min(res, axis=1)
+            scores = tf.reduce_min(res, axis=1)
         elif reduction == "ave":
-            scores = np.mean(res, axis=1)
+            scores = tf.reduce_mean(res, axis=1)
         else:
             raise Exception("Unknown reduction method")
-        return scores, np.mean(scores)
+        return scores, tf.reduce_mean(scores)
     return gen_data_distance
 
 def data_gen_distance_wrapper(flag, reduction="min", distance="Euclidean"):
     def data_gen_distance(x_eval, y_eval, x_data, y_data, n_data, scorebars, flag = flag):
         if scorebars:
             print("Calculating Data-Gen Distance")
+        scores = []
         if flag == "x":
             x = x_eval
             data = x_data
@@ -187,59 +227,83 @@ def data_gen_distance_wrapper(flag, reduction="min", distance="Euclidean"):
             data = pd.concat([x_data, y_data], axis=0)
         else:
             raise Exception("Unknown flag passed")
+            
+        num_eval = np.shape(x)[0]
         res = calc_distance(data, x, distance)
 
         if reduction == "min":
-            scores = np.min(res, axis=1)
+            scores = tf.reduce_min(res, axis=1)
         elif reduction == "ave":
-            scores = np.mean(res, axis=1)
+            scores = tf.reduce_mean(res, axis=1)
         else:
             raise Exception("Unknown reduction method")
-        return None, np.mean(scores)
+        return None, tf.reduce_mean(scores)
     return data_gen_distance
 
 def DTAI_wrapper(direction, ref, p_, a_):
     def DTAI(x_eval, y_eval, x_data, y_data, n_data, scorebars, direction=direction, ref=ref, p_=p_, a_=a_, DTAI_EPS=1e-7):
-        y_eval = np.maximum(y_eval, DTAI_EPS)
+        p_ = tf.cast(p_, "float32")
+        a_ = tf.cast(a_, "float32")
+        y_eval = tf.cast(y_eval, "float32")
         if scorebars:
             print("Calculating DTAI")
 
+        #y values must be greater than 0
+        y=tf.math.maximum(y_eval, DTAI_EPS)
         if direction=="maximize":
-            x = y_eval / ref
+            x=tf.divide(y, ref)
         elif direction=="minimize":
-            x = ref / y_eval
+            x = tf.divide(ref, y)
         else:
             raise Exception("Unknown optimization direction, expected maximize or minimize")
-        case1 = p_ * x - p_
-        p_over_a = p_ / a_
-        exponential = np.exp(a_ * (1 - x))
-        case2 = p_over_a * (1 - exponential)
-        casemask = x > 1
-        scores = case2 * casemask + case1 * (1 - casemask)
-        scores = np.sum(scores, axis=1)         
-        smax = np.sum(p_ / a_)
-        smin = -np.sum(p_)
+        case1 = tf.multiply(p_,x)-p_
+        p_over_a=tf.divide(p_,a_)
+        exponential=tf.exp(tf.multiply(a_, (1-x)))
+        case2=tf.multiply(p_over_a, (1-exponential))
+        casemask = tf.greater(x, 1)
+        casemask = tf.cast(casemask, "float32")
+        scores=tf.multiply(case2, casemask) + tf.multiply( case1, (1 - casemask))
+        scores=tf.math.reduce_sum(scores, axis=1)         
+        smax=tf.math.reduce_sum(p_/a_)
+        smin=-tf.math.reduce_sum(p_)
 
-        scores = (scores - smin) / (smax - smin)
-        return scores, np.mean(scores)
+        scores=(scores-smin)/(smax-smin)
+        return scores, tf.reduce_mean(scores)
     return DTAI
+# def minimum_target_ratio_wrapper(direction, ref):
+#     def minimum_target_ratio(x_eval, y_eval, x_data, y_data, n_data, scorebars, direction=direction, ref=ref):
+#         if scorebars:
+#             print("Calculating Minimum Target Ratio")
+#         y_eval = tf.cast(y_eval, "float32")
+#         ref = tf.cast(ref, "float32")
+#         if direction=="maximize":
+#             res = tf.divide(y_eval, ref)
+#         elif direction=="minimize":
+#             res = tf.divide(ref, y_eval)
+#         else:
+#             raise Exception("Unknown optimization direction, expected maximize or minimize")
+#         scores = tf.reduce_min(res, axis=1)
+#         return scores, tf.reduce_mean(scores)
+#     return minimum_target_ratio
 
 def weighted_target_success_rate_wrapper(direction, ref, p_):
     def weighted_target_success_rate(x_eval, y_eval, x_data, y_data, n_data, scorebars, direction=direction, ref=ref, p_=p_):
+        y_eval = tf.cast(y_eval, "float32")
         if scorebars:
             print("Calculating Weighted Target Success Rate")
-        y_eval = np.array(y_eval)
-        ref = np.array(ref)
-        p_ = np.array(p_)
-        if direction == "maximize":
-            res = y_eval > ref
-        elif direction == "minimize":
-            res = y_eval < ref
+        num_eval = y_eval[:,0]
+        y_eval = tf.cast(y_eval, "float32")
+        ref = tf.cast(ref, "float32")
+        p_ = tf.cast(p_, "float32")
+        if direction=="maximize":
+            res = tf.cast(y_eval>ref, "float32")
+        elif direction=="minimize":
+            res = tf.cast(y_eval<ref, "float32")
         else:
             raise Exception("Unknown optimization direction, expected maximize or minimize")
-        scores = res.astype(float)
-        scaled_scores = np.dot(scores, p_) / np.sum(p_)
-        return scaled_scores, np.mean(scaled_scores)
+        scores = res
+        scaled_scores = tf.matmul(scores, tf.expand_dims(p_, -1))/sum(p_)
+        return scaled_scores, tf.reduce_mean(scaled_scores)
     return weighted_target_success_rate
 
 def gen_neg_distance_wrapper(reduction = "min", distance="Euclidean"):
@@ -248,18 +312,18 @@ def gen_neg_distance_wrapper(reduction = "min", distance="Euclidean"):
             print("Calculating Gen-Neg Distance")
         res = calc_distance(x_eval, n_data, distance)
         if reduction == "min":
-            scores = np.min(res, axis=1)
+            scores = tf.reduce_min(res, axis=1)
         elif reduction == "ave":
-            scores = np.mean(res, axis=1)
+            scores = tf.reduce_mean(res, axis=1)
         else:
             raise Exception("Unknown reduction method")
-        return scores, np.mean(scores)
+        return scores, tf.reduce_mean(scores)
     return gen_neg_distance
 
 def MMD_wrapper(flag, sigma=1, batch_size=1000, num_iter=100, biased=True):
     def MMD(x_eval, y_eval, x_data, y_data, n_data, scorebars, flag=flag, sigma=sigma, biased=biased):
         if scorebars:
-            print("Calculating Maximum Mean Discrepancy")
+                print("Calculating Maximum Mean Discrepancy")
         if flag == "x":
             x = x_eval
             data = x_data
@@ -267,12 +331,12 @@ def MMD_wrapper(flag, sigma=1, batch_size=1000, num_iter=100, biased=True):
             x = y_eval
             data = y_data
         elif flag == "all":
-            x = pd.concat([x_eval, y_eval], axis=0)
+            y_eval = pd.concat([x_eval, y_eval], axis=0)
             data = pd.concat([x_data, y_data], axis=0)
         else:
             raise Exception("Unknown flag passed")
-        total = 0
-        for _ in range(num_iter):
+        total=0
+        for i in range(num_iter):
             if len(x) > batch_size:
                 X = x[np.random.randint(x.shape[0], size=batch_size), :]    
             else:
@@ -281,33 +345,37 @@ def MMD_wrapper(flag, sigma=1, batch_size=1000, num_iter=100, biased=True):
                 Y = data[np.random.randint(data.shape[0], size=batch_size), :]
             else:
                 Y = data
+            X = tf.convert_to_tensor(X, dtype=tf.float32)
+            Y = tf.convert_to_tensor(Y, dtype=tf.float32)
             gamma = 1 / (2 * sigma**2)
         
-            XX = np.dot(X, X.T)
-            XY = np.dot(X, Y.T)
-            YY = np.dot(Y, Y.T)
+            XX = tf.matmul(X, tf.transpose(X))
+            XY = tf.matmul(X, tf.transpose(Y))
+            YY = tf.matmul(Y, tf.transpose(Y))
         
-            X_sqnorms = np.diag(XX)
-            Y_sqnorms = np.diag(YY)
+            X_sqnorms = tf.linalg.diag_part(XX)
+            Y_sqnorms = tf.linalg.diag_part(YY)
         
-            K_XY = np.exp(-gamma * (
+            K_XY = tf.math.exp(-gamma * (
                     -2 * XY + X_sqnorms[:, np.newaxis] + Y_sqnorms[np.newaxis, :]))
-            K_XX = np.exp(-gamma * (
+        #             -2 * XY + tf.expand_dims(X_sqnorms, 1) + tf.expand_dims(Y_sqnorms, 0)))
+        
+            K_XX = tf.math.exp(-gamma * (
                     -2 * XX + X_sqnorms[:, np.newaxis] + X_sqnorms[np.newaxis, :]))
-            K_YY = np.exp(-gamma * (
+            K_YY = tf.math.exp(-gamma * (
                     -2 * YY + Y_sqnorms[:, np.newaxis] + Y_sqnorms[np.newaxis, :]))
             
             if biased:
-                mmd2 = np.mean(K_XX) + np.mean(K_YY) - 2 * np.mean(K_XY)
+                mmd2 = tf.math.reduce_mean(K_XX) + tf.math.reduce_mean(K_YY) - 2 * tf.math.reduce_mean(K_XY)
             else:
                 m = K_XX.shape[0]
                 n = K_YY.shape[0]
         
                 mmd2 = ((K_XX.sum() - m) / (m * (m - 1))
                     + (K_YY.sum() - n) / (n * (n - 1))
-                    - 2 * np.mean(K_XY))
-            total += mmd2
-        return None, total / num_iter
+                    - 2 * K_XY.mean())
+            total+=mmd2
+        return None, total.numpy()/num_iter
     return MMD
 
 def F_wrapper(flag, beta=1, num_clusters=20, num_angles=1001, num_runs=5, enforce_balance=False):
@@ -364,7 +432,7 @@ def AUC_wrapper(flag, num_clusters=20, num_angles=1001, num_runs=5, enforce_bala
             eval_prd.plot(prd_data, labels=None, out_path=None,legend_loc='lower left', dpi=300)
         tot = 0
         for i in range(len(precision)-1):
-            tot += (precision[i] + precision[i+1]) / 2 * (recall[i+1] - recall[i])
+            tot+=(precision[i]+precision[i+1])/2*(recall[i+1]-recall[i])
         
         return None, tot
     return calc_prd
@@ -389,6 +457,8 @@ def convex_hull_wrapper(flag):
         return None, hull.volume
     return convex_hull
 
+
+#In this metric, we assume the conditioning information is instead passed in as the performance space values (y)
 def predicted_conditioning_wrapper(reg, cond):
     def predicted_conditioning(x_eval, y_eval, x_data, y_data, n_data, scorebars, reg=reg, cond=cond):
         if scorebars:
@@ -396,10 +466,12 @@ def predicted_conditioning_wrapper(reg, cond):
         c_data = y_data
         reg.fit(x_data, c_data)
         res = reg.predict(x_eval)
-        cond = np.ones_like(res) * cond
+        cond = np.ones_like(res)*cond
         score = sklearn.metrics.mean_squared_error(res, cond)
         return None, score
     return predicted_conditioning
+
+
 
 def predicted_constraint_satisfaction_wrapper(clf):
     def predicted_constraint_satisfaction(x_eval, y_eval, x_data, y_data, n_data, scorebars, clf=clf):
@@ -408,10 +480,11 @@ def predicted_constraint_satisfaction_wrapper(clf):
         x_all = np.concatenate([x_data, n_data], axis=0)
         y_all = np.concatenate([np.ones(len(x_data)), np.zeros(len(n_data))], axis=0)
         clf.fit(x_all, y_all)
-        res = clf.predict_proba(x_eval)[:, 1]
-        return res, np.mean(res)
+        res= clf.predict_proba(x_eval)[:,1]
+        return res, tf.reduce_mean(res)
     return predicted_constraint_satisfaction
 
+#"CLF" may be a classifier or regressor
 def ML_efficacy_wrapper(clf, score):
     def ML_efficacy(x_eval, y_eval, x_data, y_data, n_data, scorebars, clf=clf, score=score):
         if scorebars:
@@ -420,5 +493,8 @@ def ML_efficacy_wrapper(clf, score):
         preds = clf.predict(x_data)
         res = score(y_data, preds)
         
-        return res, np.mean(res)
+        return res, tf.reduce_mean(res)
     return ML_efficacy
+
+
+  
